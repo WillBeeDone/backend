c294fcb6e892761906596c9832f03d0c77415d37
Artem Davidovich <chumazek@gmail.com>
2025-03-11 19:15:30 +0100

    First commit. Starting up the project buzzzzzzz

---
+++ src/main/java/de/willbeedone/backend/WillBeeDoneBackendApplication.java
@@ -0,0 +1,13 @@
+package de.willbeedone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillBeeDoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillBeeDoneBackendApplication.class, args);
+	}
+
+}
