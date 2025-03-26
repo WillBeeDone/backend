71308215a37b27c0dd169c4126d36753dd308252
valexde97 <lasco.valentin97@gmail.com>
2025-03-26 10:21:58 +0100

    security

--- src/main/java/de/willbeedone/backend/security/sec_config/SecurityConfig.java
+++ src/main/java/de/willbeedone/backend/security/sec_config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package de.willbeedone.backend.security.sec_config;
 
 import de.willbeedone.backend.security.sec_filter.TokenFilter;
-import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
