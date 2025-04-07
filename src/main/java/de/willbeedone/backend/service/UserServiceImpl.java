01064edc533866e3ce2dacd4044c59dcdbcf539c
Artem Davidovich <chumazek@gmail.com>
2025-04-07 16:51:00 +0200

    start

--- src/main/java/de/willbeedone/backend/service/UserServiceImpl.java
+++ src/main/java/de/willbeedone/backend/service/UserServiceImpl.java
@@ -27,9 +27,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
