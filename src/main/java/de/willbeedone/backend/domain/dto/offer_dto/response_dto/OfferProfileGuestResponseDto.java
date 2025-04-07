312bc530c49ddc3e6dfee6036dd301d12c37f76a
Artem Davidovich <chumazek@gmail.com>
2025-04-07 19:07:59 +0200

    notBlank

--- src/main/java/de/willbeedone/backend/domain/dto/offer_dto/response_dto/OfferProfileGuestResponseDto.java
+++ src/main/java/de/willbeedone/backend/domain/dto/offer_dto/response_dto/OfferProfileGuestResponseDto.java
@@ -2,6 +2,7 @@ package de.willbeedone.backend.domain.dto.offer_dto.response_dto;
 
 import de.willbeedone.backend.domain.dto.category_dto.CategoryDto;
 import de.willbeedone.backend.domain.dto.user_dto.response_dto.UserFilterResponseDto;
+import de.willbeedone.backend.domain.dto.user_dto.response_dto.UserProfileResponseDto;
 import de.willbeedone.backend.domain.entity.ImageGallery;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
@@ -30,7 +31,6 @@ public class OfferProfileGuestResponseDto {
     @Schema(description = "Short offer description", example = "Super sexy plumber will fix your pipes.")
     private String title;
 
-    @NotNull
     @Schema(description = "Offer categoryDto")
     private CategoryDto categoryDto;
 
@@ -47,23 +47,13 @@ public class OfferProfileGuestResponseDto {
     @Schema(description = "Photo gallery of work results")
     private Set<ImageGallery> images;
 
-    @NotBlank(message = "Description cannot be empty")
-    @Email(message = "Invalid email format")
-    @Schema(description = "User email", example = "user@example.com")
-    private String email;
-
-    @NotBlank(message = "Description cannot be empty")
-    @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Invalid phone number format")
-    @Schema(description = "User phone number", example = "+1234567890")
-    private String phoneNumber;
-
     @NotNull
-    @Schema(description = "The fields from User: first name, last name, location, profile picture")
-    private UserFilterResponseDto userFilterResponseDto;
+    @Schema(description = "The fields from User: first name, last name, location, profile picture, email, phoneNumber")
+    private UserProfileResponseDto userProfileResponseDto;
 
     @Override
     public String toString() {
 
-        return String.format("OfferProfileGuestResponseDto: id - %d, title - %s, categoryResponseDto - %s, pricePerHour - %.2f, description - %s, images - %s, email -%s, phoneNumber - %s, userFilterResponseDto - %s", id, title, categoryDto, pricePerHour, description, images, email, phoneNumber, userFilterResponseDto);
+        return String.format("OfferProfileGuestResponseDto: id - %d, title - %s, categoryResponseDto - %s, pricePerHour - %.2f, description - %s, images - %s, userProfileResponseDto - %s", id, title, categoryDto, pricePerHour, description, images, userProfileResponseDto);
     }
 }
