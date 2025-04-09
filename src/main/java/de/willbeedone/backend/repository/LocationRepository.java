3871516f99561ab5e2bdadc23c3b83ff0ecdaeca
Artem Davidovich <chumazek@gmail.com>
2025-04-09 17:56:52 +0200

    cityName bad request

--- src/main/java/de/willbeedone/backend/repository/LocationRepository.java
+++ src/main/java/de/willbeedone/backend/repository/LocationRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Optional<Location> findByCityName(String cityName);
+
+    boolean existsByCityName(String cityName);
 }
