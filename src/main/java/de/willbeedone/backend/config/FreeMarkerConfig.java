9529b05ef78f6abe50a3e6616e30d8c9cd42387b
LutsDM <lutsdm@gmail.com>
2025-07-11 21:09:02 +0200

    freemarkerConfig

---
+++ src/main/java/de/willbeedone/backend/config/FreeMarkerConfig.java
@@ -0,0 +1,16 @@
+package de.willbeedone.backend.config;
+
+import freemarker.template.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class FreeMarkerConfig {
+
+    @Bean
+    public Configuration freemarkerConfiguration() throws Exception {
+        Configuration config = new Configuration(Configuration.VERSION_2_3_31);
+        config.setClassForTemplateLoading(this.getClass(), "/mail/");
+        config.setDefaultEncoding("UTF-8");
+        return config;
+    }
+}
