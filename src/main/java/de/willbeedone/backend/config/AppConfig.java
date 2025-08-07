06b548ff68fc792645b621c2c911fba321c80cab
LutsDM <lutsdm@gmail.com>
2025-08-07 17:05:02 +0200

    optional AmazonS3 in the ImageServiceImpl

--- src/main/java/de/willbeedone/backend/config/AppConfig.java
+++ src/main/java/de/willbeedone/backend/config/AppConfig.java
@@ -11,26 +11,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public AmazonS3 doClient(DOProperties properties){
-
-        AWSCredentials credentials = new BasicAWSCredentials(
-                properties.getAccessKey(),
-                properties.getSecretKey()
-        );
-
-        AwsClientBuilder.EndpointConfiguration endpointConfig =
-                new AwsClientBuilder.EndpointConfiguration(
-                        properties.getEndpoint(),
-                        properties.getRegion()
-                );
-
-        AmazonS3ClientBuilder clientBuilder = AmazonS3ClientBuilder
-                .standard()
-                .withEndpointConfiguration(endpointConfig)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials));
-
-        return clientBuilder.build();
-
-    }
+//    @Bean
+//    public AmazonS3 doClient(DOProperties properties){
+//
+//        AWSCredentials credentials = new BasicAWSCredentials(
+//                properties.getAccessKey(),
+//                properties.getSecretKey()
+//        );
+//
+//        AwsClientBuilder.EndpointConfiguration endpointConfig =
+//                new AwsClientBuilder.EndpointConfiguration(
+//                        properties.getEndpoint(),
+//                        properties.getRegion()
+//                );
+//
+//        AmazonS3ClientBuilder clientBuilder = AmazonS3ClientBuilder
+//                .standard()
+//                .withEndpointConfiguration(endpointConfig)
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials));
+//
+//        return clientBuilder.build();
+//
+//    }
 }
